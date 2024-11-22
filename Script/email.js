76fef4a32564dc315082d19c4441946f8c8ac0b4
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2024-11-22 19:10:34 +0800

    Add files via upload

--- Script/email.js
+++ Script/email.js
@@ -1,5 +1,10 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+let isEmailCooldown = false;
+
+function sendEmail() {
+    if (isEmailCooldown) {
+        alert('Cooldown!');
+        return;
+    }
 
     var username = document.getElementById('contact-username').value.trim();
     var message = document.getElementById('contact-message').value.trim();
@@ -9,29 +14,30 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         return;
     }
 
-    grecaptcha.execute('6Le2xYYqAAAAALl5p4hUU-NUT3mdVUdgAjZIL_QU', { action: 'submit' }).then(function(token) {
-        var data = {
-            service_id: 'deaditya',
-            template_id: 'temp_email',
-            user_id: '3rOnyc1hK0Q6qfdk_',
-            template_params: {
-                'username': username,
-                'message': message,
-                'g-recaptcha-response': token
-            }
-        };
+    var data = {
+        service_id: 'deaditya',
+        template_id: 'temp_email',
+        user_id: '3rOnyc1hK0Q6qfdk_',
+        template_params: {
+            'username': username,
+            'message': message
+        }
+    };
 
-        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-            type: 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        }).done(function() {
-            alert('Pesan kamu telah terkirim');
-            document.getElementById("contact-form").reset();
-        }).fail(function(error) {
-            alert('Oops... ' + JSON.stringify(error));
-        });
-    }).catch(function(error) {
-        alert('reCAPTCHA failed: ' + error);
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function() {
+        alert('Pesan kamu telah terkirim');
+        document.getElementById("contact-form").reset();
+    }).fail(function(error) {
+        alert('Oops... ' + JSON.stringify(error));
     });
-});
+
+    isEmailCooldown = true;
+
+    setTimeout(() => {
+        isEmailCooldown = false;
+    }, 60000);
+}
