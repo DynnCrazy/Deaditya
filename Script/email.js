fceb6e37cf6b6621cbe30e7b93f371288cee37d8
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2024-11-22 18:45:59 +0800

    Add files via upload

--- Script/email.js
+++ Script/email.js
@@ -1,4 +1,6 @@
-function sendEmail() {
+function sendEmail(event) {
+    event.preventDefault();
+
     var username = document.getElementById('contact-username').value.trim();
     var message = document.getElementById('contact-message').value.trim();
 
@@ -7,24 +9,27 @@ function sendEmail() {
         return;
     }
 
-    var data = {
-        service_id: 'deaditya',
-        template_id: 'temp_email',
-        user_id: '3rOnyc1hK0Q6qfdk_',
-        template_params: {
-            'username': username,
-            'message': message
-        }
-    };
+    grecaptcha.execute('6Le2xYYqAAAAALl5p4hUU-NUT3mdVUdgAjZIL_QU', { action: 'submit' }).then(function(token) {
+        var data = {
+            service_id: 'deaditya',
+            template_id: 'temp_email',
+            user_id: '3rOnyc1hK0Q6qfdk_',
+            template_params: {
+                'username': username,
+                'message': message,
+                'g-recaptcha-response': token
+            }
+        };
 
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-        type: 'POST',
-        data: JSON.stringify(data),
-        contentType: 'application/json'
-    }).done(function() {
-        alert('Pesan kamu telah terkirim');
-        document.getElementById("contact-form").reset();
-    }).fail(function(error) {
-        alert('Oops... ' + JSON.stringify(error));
+        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json'
+        }).done(function() {
+            alert('Pesan kamu telah terkirim');
+            document.getElementById("contact-form").reset();
+        }).fail(function(error) {
+            alert('Oops... ' + JSON.stringify(error));
+        });
     });
 }
