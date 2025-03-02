786a854b8c546c10d02a754ddbd95dabe9e27e77
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2025-03-02 22:38:13 +0800

    Delete Script directory

--- Script/id-js.js
+++
@@ -1,7 +0,0 @@
-var id_contstats = document.getElementById("contstats");
-var id_valuepencapaiandidapatkan = document.getElementById("valuepencapaiandidapatkan");
-var id_statswaktuhidup = document.getElementById("statswaktuhidup");
-
-var id_btnbar = document.getElementById("btnbar");
-var id_menunb = document.getElementById("menunb");
-var id_contdivmenud = document.getElementById("contdivmenud");
