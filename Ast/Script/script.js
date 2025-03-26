94bb21267352288f80316116eeee8c3643cb22d7
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2025-03-26 12:34:44 +0800

    Add files via upload

--- Ast/Script/script.js
+++ Ast/Script/script.js
@@ -13,3 +13,47 @@ function spawnEmoji() {
     }, 2000);
 }
   
+
+var isLightMode = true;
+
+function changeTheme() {
+    if (isLightMode) {
+        document.getElementById("webthemebtn").innerHTML = '<span class="fas fa-toggle-on" style="cursor: pointer;"></span>';
+        document.querySelectorAll(".mainfooter, .containeraboutme, .containerfuture, .containerlink").forEach(el => {
+            el.style.backgroundColor = "#001b2b";
+        });
+        document.querySelectorAll(".submainfooter a").forEach(clr => {
+            clr.style.color = "white";
+        });
+        document.querySelectorAll(".garispemisah").forEach(wk => {
+            wk.style.backgroundColor = "#001b2b";
+        });
+        document.querySelectorAll(".mainfooter").forEach(ss => {
+            ss.style.borderTop = "2px solid #001b2b";
+        });
+        document.body.style.backgroundColor = "#00111b";
+        document.body.style.backgroundImage = "radial-gradient(circle,rgb(0, 23, 36) 2px, transparent 2px)";
+        document.body.style.color = "white";
+        isLightMode = false;
+    } else {
+        document.getElementById("webthemebtn").innerHTML = '<span class="fas fa-toggle-off" style="cursor: pointer;"></span>';
+        document.querySelectorAll(".mainfooter, .containeraboutme, .containerfuture, .containerlink").forEach(el => {
+            el.style.backgroundColor = "#f4f4f4";
+        });
+        document.querySelectorAll(".submainfooter a").forEach(clr => {
+            clr.style.color = "#001b2b";
+        });
+        document.querySelectorAll(".garispemisah").forEach(wk => {
+            wk.style.backgroundColor = "#dfdfdf";
+        });
+        document.querySelectorAll(".mainfooter").forEach(ss => {
+            ss.style.borderTop = "2px solid #dfdfdf";
+        });
+        document.body.style.backgroundColor = "white";
+        document.body.style.backgroundImage = "radial-gradient(circle, rgb(238, 238, 238) 2px, transparent 2px)";
+        document.body.style.color = "#00111b";
+        isLightMode = true;
+    }
+}
+
+document.getElementById("copyinftr").innerHTML = "Copyright &copy 2025";
