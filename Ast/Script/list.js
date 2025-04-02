d7b632513f604c40b64aa41dcf95331e261a15ea
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2025-04-02 10:38:06 +0800

    Add files via upload

--- Ast/Script/list.js
+++ Ast/Script/list.js
@@ -6,15 +6,19 @@ var dataGamesList_2;
 var dataGamesList_3;
 
 function prosesData(data) {
-    console.log("Data sudah siap:", data);
+    // console.log("Data sudah siap:", data);
     dataMovieList = dataGlobal.movie;
     dataGamesList = dataGlobal.games;
 
-    if (data.games && Array.isArray(data.games)) {
-        dataGamesList = data.games.sort((a, b) => a.nama.localeCompare(b.nama));
-        console.log(dataGamesList);
+    if (data.movie && Array.isArray(data.movie)) {
+        dataMovieList = data.movie.sort((a, b) => a.nama.localeCompare(b.nama));
     } else {
-        console.error("Data games tidak ditemukan atau bukan array");
+        dataMovieList = [];
+    }
+
+    if (data.games && Array.isArray(data.games)) {
+        dataGamesList = data.games.sort((c, d) => c.nama.localeCompare(d.nama));
+    } else {
         dataGamesList = [];
     }
 }
@@ -31,6 +35,8 @@ fetch("/Ast/Data/mylist.json")
 
 function updateMovieList() {
     var tempMovieList = "";
+    console.log("Saya pernah menonton " + dataMovieList.length + " movie!");
+
 
     for (let movieF = 0; movieF < dataMovieList.length; movieF++) {
         tempMovieList += `<div class="contimgmymovielist" onclick="alert('${dataMovieList[movieF].nama} (${dataMovieList[movieF].tahun})')">
@@ -46,7 +52,15 @@ function updateGamesList() {
     var tempGamesList_pg1 = "";
     var tempGamesList_pg2 = "";
     var tempGamesList_pg3 = "";
+
+    for (let removeKosongGms = dataGamesList.length - 1; removeKosongGms >= 0; removeKosongGms--) {
+        if (dataGamesList[removeKosongGms].nama === "") {
+            dataGamesList.splice(removeKosongGms, 1);
+        }
+    }
+
     gamesPerPage = Math.round(dataGamesList.length / 3);
+    console.log("Saya pernah memainkan " + dataGamesList.length + " game!");
 
     var kondisigmspglist_1 = gamesPerPage;
     var kondisigmspglist_2 = gamesPerPage + gamesPerPage - 1;
