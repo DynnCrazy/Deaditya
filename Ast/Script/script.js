5957855ad90d7378ab189c6e4b9421399c50d431
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2025-04-15 00:07:54 +0800

    Add files via upload

--- Ast/Script/script.js
+++ Ast/Script/script.js
@@ -21,6 +21,7 @@ function changeTheme() {
         document.getElementById("webthemebtn").innerHTML = '<span class="fas fa-toggle-on" style="cursor: pointer;"></span>';
         document.querySelectorAll(".containerdtl, .mainfooter, .containeraboutme, .containerfuture, .containerlink").forEach(el => {
             el.style.backgroundColor = "#001b2b";
+            el.style.boxShadow = "0 0 20px 15px #00111b";
             el.style.border = "none";
         });
         document.querySelectorAll(".submainfooter a").forEach(clr => {
@@ -31,18 +32,24 @@ function changeTheme() {
         });
         document.querySelectorAll(".garispemisah").forEach(wk => {
             wk.style.backgroundColor = "#001b2b";
+            wk.style.boxShadow = "0 0 20px 15px #00111b";
         });
         document.querySelectorAll(".mainfooter").forEach(ss => {
             ss.style.borderTop = "2px solid #001b2b";
+            ss.style.boxShadow = "0 0 20px 15px #00111b";
         });
         document.body.style.backgroundColor = "#00111b";
-        document.body.style.backgroundImage = "radial-gradient(circle,rgb(0, 23, 36) 2px, transparent 2px)";
+        document.body.style.backgroundImage = `linear-gradient(to right, #001521 1px, transparent 1px),
+                                               linear-gradient(to bottom, #001521 1px, transparent 1px)`;
+        document.body.style.backgroundSize = "50px 50px";
+
         document.body.style.color = "white";
         isLightMode = false;
     } else {
         document.getElementById("webthemebtn").innerHTML = '<span class="fas fa-toggle-off" style="cursor: pointer;"></span>';
         document.querySelectorAll(".containerdtl, .mainfooter, .containeraboutme, .containerfuture, .containerlink").forEach(el => {
-            el.style.backgroundColor = "rgb(252, 252, 252)";
+            el.style.backgroundColor = "#fcfcfc";
+            el.style.boxShadow = "0 0 20px 15px white";
             el.style.border = "1px solid #e6e6e6";
         });
         document.querySelectorAll(".submainfooter a").forEach(clr => {
@@ -53,13 +60,18 @@ function changeTheme() {
         });
         document.querySelectorAll(".garispemisah").forEach(wk => {
             wk.style.backgroundColor = "#dfdfdf";
+            wk.style.boxShadow = "0 0 20px 15px white";
         });
         document.querySelectorAll(".mainfooter").forEach(ss => {
             ss.style.borderTop = "2px solid #dfdfdf";
+            ss.style.boxShadow = "0 0 20px 15px white";
         });
         document.body.style.backgroundColor = "white";
-        document.body.style.backgroundImage = "radial-gradient(circle, rgb(250, 250, 250) 2px, transparent 2px)";
-        document.body.style.color = "#00111b";
+        document.body.style.backgroundImage = `linear-gradient(to right, #f9f9f9 1px, transparent 1px),
+                                               linear-gradient(to bottom, #f9f9f9 1px, transparent 1px)`;
+        document.body.style.backgroundSize = "50px 50px";
+
+        document.body.style.color = "#2b2b2b";
         isLightMode = true;
     }
 }
