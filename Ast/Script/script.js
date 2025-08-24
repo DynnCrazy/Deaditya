94cf0a5d198ee9caaa62e4fa2b83f8c329560123
Dyn Crazy <121717799+DynnCrazy@users.noreply.github.com>
2025-08-24 22:54:12 +0800

    Add files via upload

--- Ast/Script/script.js
+++ Ast/Script/script.js
@@ -1,8 +1,10 @@
-if (window.innerWidth < 228) {
+if (window.innerWidth < 280) {
     alert("Ukuran lebar layar kamu terlalu kecil, tidak bisa menampilkan halaman konten dengan semestinya!")
 } else if (window.innerHeight <= 350) {
     alert("Ukuran tinggi layar kamu terlalu kecil, tidak bisa menampilkan halaman konten dengan semestinya!")
 }
+
+const today = new Date();
 
 function goToLink(link) {
     if (link.includes("#")) {
@@ -38,4 +40,43 @@ function back() {
 
 function comingSoon() {
     alert("Segera hadir!");
+}
+
+async function getData(dataId) {
+        var data = '';
+
+        try {
+            if (dataId === "0") {
+                const response = await fetch("https://datadc.netlify.app/data/quotes/quotes.json");
+                var data = await response.json();
+                const daysSinceEpoch = Math.floor(today.getTime() / (1000 * 60 * 60 * 24));
+                const index = daysSinceEpoch % data.quotes.length;
+
+                document.getElementById("quotes_web").innerText = `"${data.quotes[index].q_id}"\n- ${data.quotes[index].a}`;
+
+            } else if (dataId === "1") {
+                const response = await fetch("https://datadc.netlify.app/data/project/desain.json");
+                var data = await response.json();
+                var temp = '';
+
+                for (let i = 0; i < data.desain.length; i++) {
+                    temp += `
+                        <div class="item mb-4 relative break-inside-avoid overflow-hidden">
+                            <img src="${data.desain[i].image}" alt="${data.desain[i].nameortitle}">
+                            <div class="item-overlay">${data.desain[i].nameortitle}</div>
+                        </div>`;
+                }
+
+                document.getElementById("masonry-grid").innerHTML = temp;
+            }
+
+        } catch (error) {
+            console.error("Gagal ambil data:", error);
+        }
+}
+
+if (window.location.pathname === "/project.html") {
+    getData("1");
+} else {
+    getData("0");
 }
